be286d8bea562fa330254b2ba4cc05494501bc48
Luracast <arul@luracast.com>
2017-12-09 18:20:25 +0800

    basic structure of Restler

---
+++ src/Restler.php
@@ -0,0 +1,37 @@
+<?php
+
+
+class Restler
+{
+    protected function get()
+    {
+    }
+
+    protected function route()
+    {
+    }
+
+    protected function negotiate()
+    {
+    }
+
+    protected function authenticate()
+    {
+    }
+
+    protected function validate()
+    {
+    }
+
+    protected function call()
+    {
+    }
+
+    protected function compose()
+    {
+    }
+
+    protected function respond()
+    {
+    }
+}
