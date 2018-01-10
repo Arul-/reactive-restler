864da9bd9abac0aa5336f8147f83dc756554bf89
Luracast <arul@luracast.com>
2018-01-10 18:52:05 +0800

    try to resolve config parameters to contractor

--- src/Container.php
+++ src/Container.php
@@ -1,11 +1,20 @@
 <?php namespace Luracast\Restler;
 
 
+use Closure;
 use Illuminate\Container\Container as LaravelContainer;
+use Luracast\Restler\Filters\RateLimiter;
 use Psr\Container\ContainerInterface;
+use ReflectionParameter;
 
 class Container extends LaravelContainer implements ContainerInterface
 {
+    public function __construct(array $aliases = [], array $abstractAliases = [])
+    {
+        $this->aliases = $aliases;
+        $this->abstractAliases = $abstractAliases;
+    }
+
     public function has($id)
     {
         return parent::has($id);
@@ -14,6 +23,29 @@ class Container extends LaravelContainer implements ContainerInterface
     public function get($id)
     {
         return parent::get($id);
+    }
+
+    protected function resolvePrimitive(ReflectionParameter $parameter)
+    {
+        if (!is_null($concrete = $this->getContextualConcrete('$' . $parameter->name))) {
+            return $concrete instanceof Closure ? $concrete($this) : $concrete;
+        }
+
+        if ($parameter->getType()->getName() == 'array') {
+            
+            $configClasses = [
+                'defaults' => Defaults::class,
+                'router' => Router::class,
+                'ratelimiter' => RateLimiter::class,
+                'passthrough'=> PassThrough::class
+            ];
+        }
+
+        if ($parameter->isDefaultValueAvailable()) {
+            return $parameter->getDefaultValue();
+        }
+
+        $this->unresolvablePrimitive($parameter);
     }
 
 }
