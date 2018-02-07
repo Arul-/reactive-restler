6901b5642fb46e5da6242c14f804c2bb5c6a843f
Luracast <arul@luracast.com>
2018-02-07 21:51:11 +0530

    test for CORS preflight

---
+++ public/test_cors.php
@@ -0,0 +1,40 @@
+<?php
+
+use Luracast\Restler\App;
+use Luracast\Restler\Reactler;
+use Luracast\Restler\Router;
+use Luracast\Restler\Utils\Dump;
+use RingCentral\Psr7\ServerRequest;
+
+include __DIR__ . "/../vendor/autoload.php";
+
+class Home
+{
+    function index()
+    {
+        return true;
+    }
+}
+
+App::$crossOriginResourceSharing = true;
+Router::addAPI(Home::class, '');
+
+$h = new Reactler();
+
+$request = new ServerRequest('OPTIONS', 'http://localhost:4000', [
+    'Access-Control-Request-Method' => 'GET',
+    'Origin' => 'http://localhost:3000',
+    'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+    'Access-Control-Request-Headers' => 'authorization,content-type',
+    'Accept' => '*/*',
+    'DNT' => 1,
+    'Referer' => 'http://localhost:3000/login',
+    'Accept-Encoding' => 'gzip, deflate, br',
+    'Accept-Language' => 'en-US,en;q=0.9,ta;q=0.8'
+], '');
+
+$response = $h->handle($request);
+
+echo Dump::response($response);
+
+
