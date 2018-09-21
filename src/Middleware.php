453128d3e3907b88f802a783dca38ed95037d117
Arul Kumaran <arul@logicalsteps.net>
2018-09-21 17:32:28 +0800

    Support for PSR-15 MiddlewareInterface

--- src/Middleware.php
+++ src/Middleware.php
@@ -2,9 +2,11 @@
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Throwable;
 
-class Middleware implements RequestHandlerInterface
+class Middleware implements RequestHandlerInterface, MiddlewareInterface
 {
     /**
      * React PHP Middleware interface
@@ -18,12 +20,25 @@ class Middleware implements RequestHandlerInterface
     }
 
     /**
-     * PSR 15 Request Handler Interface to take a request and return a response.
+     * PSR-15 Request Handler Interface to take a request and return a response.
      * @param ServerRequestInterface $request
      * @return ResponseInterface
      */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         return (new Reactler)->handle($request);
+    }
+
+    /**
+     * PSR-15 Middleware Interface to process an incoming server request and return a response,
+     * optionally delegating response creation to a handler.
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        try {
+            return (new Reactler)->handle($request);
+        } catch (Throwable $throwable) {
+            return $handler->handle($request->withAttribute('Exception', $throwable));
+        }
     }
 }
