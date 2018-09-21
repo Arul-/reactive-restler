c5a28f2df9f44553332dc102b2bc56d9fee4ce0a
Arul Kumaran <arul@logicalsteps.net>
2018-09-21 16:39:14 +0800

    Support for PSR-15 RequestHandlerInterface

--- src/Middleware.php
+++ src/Middleware.php
@@ -1,12 +1,29 @@
 <?php namespace Luracast\Restler;
 
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
-class Middleware
+class Middleware implements RequestHandlerInterface
 {
+    /**
+     * React PHP Middleware interface
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
     public function __invoke(ServerRequestInterface $request)
     {
         echo '      ' . $request->getMethod() . ' ' . $request->getUri()->getPath() . PHP_EOL;
+        return (new Reactler)->handle($request);
+    }
+
+    /**
+     * PSR 15 Request Handler Interface to take a request and return a response.
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
         return (new Reactler)->handle($request);
     }
 }
