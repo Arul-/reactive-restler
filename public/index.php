e9846e305ccda906cf219646620c22ae22b3596e
Arul Kumaran <arul@logicalsteps.net>
2018-09-21 23:35:59 +0800

    index.php to run restler though Apache web server along with htaccess file. All behat tests are passing. `api/common/store` needs to be given proper file permissions to get it working

--- public/index.php
+++ public/index.php
@@ -11,6 +11,46 @@ $request = ServerRequest::fromGlobals();
 if (isset($GLOBALS['HTTP_RAW_REQUEST_DATA'])) {
     $request = $request->withBody(stream_for($GLOBALS['HTTP_RAW_REQUEST_DATA']));
 }
+
+
+function removeCommonPath($fromPath, $usingPath, $char = '/')
+{
+    if (empty($fromPath)) {
+        return '';
+    }
+    $fromPath = explode($char, $fromPath);
+    $usingPath = explode($char, $usingPath);
+    while (count($usingPath)) {
+        if (count($fromPath) && $fromPath[0] == $usingPath[0]) {
+            array_shift($fromPath);
+        } else {
+            break;
+        }
+        array_shift($usingPath);
+    }
+    return implode($char, $fromPath);
+}
+
+$fullPath = urldecode($_SERVER['REQUEST_URI']);
+$uri = $request->getUri();
+$path = removeCommonPath(
+    $fullPath,
+    $_SERVER['SCRIPT_NAME']
+);
+$path = rtrim(strtok($path, '?'), '/');
+$debug = false;
+if ($debug) {
+    echo var_export($_SERVER);
+    echo PHP_EOL . PHP_EOL;
+    echo var_export($request->getServerParams());
+    echo PHP_EOL . PHP_EOL;
+    echo Dump::request($request);
+    echo "Path: $path\n\n";
+}
+
+$uri = $uri->withPath($path);
+$request = $request->withUri($uri);
+
 $r = new Reactler();
 $response = $r->handle($request);
-echo Dump::response($response);
+echo Dump::response($response, $debug);
