13506b7a23c1635408438dca56944d37eff8430c
Arul Kumaran <arul@logicalsteps.net>
2018-11-09 22:30:52 +0800

    Simplifies workman server by making use of the new restler protocol

--- public/index_workman.php
+++ public/index_workman.php
@@ -1,29 +1,10 @@
 <?php declare(strict_types=1);
 
-use Luracast\Restler\Defaults;
-use Luracast\Restler\Reactler;
-use Luracast\Restler\Utils\Dump;
-use Psr\Http\Message\ResponseInterface;
-use Workerman\Connection\TcpConnection;
 use Workerman\Worker;
 
 require __DIR__ . '/../api/bootstrap.php';
 
-Defaults::$returnResponse = true;
-
-// #### http worker ####
-$http_worker = new Worker("http://0.0.0.0:8080");
-
-// 4 processes
-$http_worker->count = 4;
-
-// Emitted when data received
-$http_worker->onMessage = function (TcpConnection $connection, $data) {
-    $r = new Reactler();
-    $r->handle()->then(function (ResponseInterface $response) use ($connection) {
-        $connection->close(Dump::response($response), true);
-    });
-};
-
+$worker = new Worker('restler://0.0.0.0:8080');
+$worker->count = 4;
 // run all workers
 Worker::runAll();
