4e6d0deb3cf77b5af37b1b4364542758f462c8ca
Arul Kumaran <arul@logicalsteps.net>
2018-12-18 11:50:46 +0800

    Adds some array methods using __call and documents them

--- src/StaticProperties.php
+++ src/StaticProperties.php
@@ -5,11 +5,35 @@ namespace Luracast\Restler;
 use ArrayObject;
 use BadMethodCallException;
 
+/**
+ * Class StaticProperties
+ * @package Luracast\Restler
+ *
+ * @method array chunk(array $array, int $size, bool $preserve_keys = false) Split an array into chunks
+ * @method array column(array $input, mixed $column_key, mixed $index_key = null) Return the values from a single
+ * column in the input array
+ * @method array combine(array $keys, array $values) Creates an array by using one array for keys and another for
+ * its values
+ *
+ */
 class StaticProperties extends ArrayObject
 {
     public function __construct($input = array())
     {
         parent::__construct($input, self::ARRAY_AS_PROPS);
+    }
+
+
+    public function __call($name, $argv)
+    {
+        switch ($name) {
+            case 'chunk':
+            case 'column':
+            case 'combine':
+                $func = "array_$name";
+                return call_user_func_array($func, array_merge(array($this->getArrayCopy()), $argv));
+        }
+        throw new BadMethodCallException(__CLASS__ . '->' . $name);
     }
 
     public static function fromArray(array $input): self
