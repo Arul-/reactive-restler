8686db9a212eb72d72d3dc0204938a5d786bab11
Arul Kumaran <arul@logicalsteps.net>
2018-12-31 15:13:39 +0800

    more array methods enabled and documented

--- src/ArrayObject.php
+++ src/ArrayObject.php
@@ -6,11 +6,15 @@ use ArrayObject as Base;
 use BadMethodCallException;
 
 /*
- * @method array chunk(array $array, int $size, bool $preserve_keys = false) Split an array into chunks
- * @method array column(array $input, mixed $column_key, mixed $index_key = null) Return the values from a single
+ * @method array chunk(int $size, bool $preserve_keys = false) Split an array into chunks
+ * @method array column(mixed $column_key, mixed $index_key = null) Return the values from a single
  * column in the input array
  * @method array combine(array $keys, array $values) Creates an array by using one array for keys and another for
  * its values
+ * @method array slice(int $offset, int $length = null, bool $preserve_keys = false) Extract a slice as an array
+ * @method self splice(int $offset, int $length = count($input), mixed $replacement = array()) Remove a portion of the arrayObject and replace it with something else
+ * @method mixed shift() Shift an element off the beginning of arrayObject
+ * @method mixed pop() Pop the element off the end of arrayObject
  */
 
 class ArrayObject extends Base
@@ -23,13 +27,35 @@ class ArrayObject extends Base
 
     public function __call($name, $argv)
     {
+        $found = false;
+        $modifier = false;
+        $func = null;
         switch ($name) {
+            //MOD functions
+            case 'shift':
+            case 'pop':
+            case 'splice':
+                $found = true;
+                $modifier = true;
+                break;
+            case 'slice':
             case 'chunk':
             case 'column':
             case 'combine':
-                //TODO: add more array methods and document them
+                $found = true;
+                break;
+        }
+        //TODO: add more array methods and document them
+        if ($found) {
+            if (!$func) {
                 $func = "array_$name";
-                return call_user_func_array($func, array_merge(array($this->getArrayCopy()), $argv));
+            }
+            $result = call_user_func_array($func, array_merge(array($this->getArrayCopy()), $argv));
+            if (!$modifier) {
+                return $result;
+            }
+            $this->exchangeArray($result);
+            return $this;
         }
         throw new BadMethodCallException(__CLASS__ . '->' . $name);
     }
