d38734ec11aca63b42e1a4058ae0e397fc471a59
Arul Kumaran <arul@luracast.com>
2019-01-29 17:40:17 +0800

    Bringing back full body parsing for now

--- interop/Workerman/Psr7.php
+++ interop/Workerman/Psr7.php
@@ -11,35 +11,6 @@ use Workerman\Connection\TcpConnection;
 class Psr7 extends Http
 {
     /**
-     * Check the integrity of the package.
-     *
-     * @param string $recv_buffer
-     * @param TcpConnection $connection
-     * @return int
-     */
-    public static function input($recv_buffer, TcpConnection $connection)
-    {
-        static $seperator = "\r\n\r\n";
-        if (!strpos($recv_buffer, $seperator)) {
-            // Judge whether the package length exceeds the limit.
-            if (strlen($recv_buffer) >= $connection->maxPackageSize) {
-                $connection->close();
-            }
-            return 0;
-        }
-
-        list($header, $body) = explode($seperator, $recv_buffer, 2);
-        $method = substr($header, 0, strpos($header, ' '));
-
-        if (in_array($method, static::$methods)) {
-            return strlen($header);
-        } else {
-            $connection->send("HTTP/1.1 400 Bad Request$seperator", true);
-            return 0;
-        }
-    }
-
-    /**
      * Parse $_POST、$_GET、$_COOKIE.
      *
      * @param string $recv_buffer
@@ -49,23 +20,9 @@ class Psr7 extends Http
      */
     public static function decode($recv_buffer, TcpConnection $connection)
     {
-        $connection->onMessage = function ($connection, $msg) {
-            echo '$ ' . $msg . PHP_EOL;
-        };
-        $stream = $connection->getSocket();
-        $connection->resumeRecv();
-        $http_body = fread($stream, 10);
-        /*
-        echo '--------------------------' . PHP_EOL;
-        echo $recv_buffer . PHP_EOL . PHP_EOL;
-        echo '# ' . $http_body . PHP_EOL;
-        echo '--------------------------' . PHP_EOL . PHP_EOL;
-        */
-        //list($http_header, $http_body) = explode("\r\n\r\n", $recv_buffer, 2);
-        $http_header = $recv_buffer;
+        list($http_header, $http_body) = explode("\r\n\r\n", $recv_buffer, 2);
         $header_strings = explode("\r\n", $http_header);
         list($method, $uri, $protocol) = explode(' ', $header_strings[0]);
-        echo "$method $uri\n";
         array_shift($header_strings);
         $headers = [];
         foreach ($header_strings as $header_string) {
