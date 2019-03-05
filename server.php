efcc95c4bec25367238f8a2be8df7e74cd6dc003
Arul Kumaran <arul@logicalsteps.net>
2019-03-05 22:01:22 +0800

    Ability to run `composer serve` to run the php dev server

---
+++ server.php
@@ -0,0 +1,11 @@
+<?php
+define('WEB_FOLDER', __DIR__ . '/public/');
+$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+if (file_exists(WEB_FOLDER . $request)) {
+    return false; // serve the requested resource as-is.
+} else {
+    $_SERVER['SCRIPT_NAME'] = '/index.php';
+    $_SERVER['PATH_INFO'] = $_SERVER['REQUEST_URI'];
+    $_SERVER['PHP_SELF'] = $_SERVER['SCRIPT_NAME'] . ($_SERVER['REQUEST_URI'] === '/' ? '' : $_SERVER['REQUEST_URI']);
+    include WEB_FOLDER . 'index.php';
+}
