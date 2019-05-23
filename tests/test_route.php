54ed42364012bfd48764d9a5ffd73044c3719e4e
Arul Kumaran <arul@logicalsteps.net>
2019-05-23 23:00:10 +0800

    Shows creating a route with comment parser metadata

--- tests/test_route.php
+++ tests/test_route.php
@@ -2,20 +2,41 @@
 
 use Luracast\Restler\Data\Route;
 use Luracast\Restler\Data\Param;
+use Luracast\Restler\Utils\CommentParser;
 
 include __DIR__ . "/../vendor/autoload.php";
 
-
-$route = new Route();
-
+/**
+ * @param int $a {@min 3}{@max 5}
+ * @param int $b
+ * @return float|int
+ */
 $action = function (int $a, int $b) {
     return $a / $b;
 };
 
+$f = new ReflectionFunction($action);
+$c = CommentParser::parse($f->getDocComment());
+//var_dump($c);
 
+$route = Route::parse(['metadata' => $c]);
 $route->action = $action;
+/*
+
+$route = new Route();
+$route->action = $action;
+$ps = $f->getParameters();
+foreach ($ps as $p) {
+    $route->addParameter(Param::parse([
+        'name' => $p->name,
+        'type' => $p->hasType() ? $p->getType()->getName() : null
+    ]));
+}
+*/
 
 /*
+$route = new Route();
+$route->action = $action;
 $a = new Param();
 $a->type = 'int';
 $a->name = 'a';
@@ -26,25 +47,16 @@ $b->type = 'int';
 $b->name = 'b';
 $route->addParameter($b);
 */
-$f = new ReflectionFunction($action);
-$ps = $f->getParameters();
-foreach ($ps as $p) {
-    $route->addParameter(Param::parse([
-        'name' => $p->name,
-        'type' => $p->hasType() ? $p->getType()->getName() : null
-    ]));
-}
-
-print_r($route->call([4, 2]));
-echo PHP_EOL;
-
-print_r($route->call(['b' => 4, 'a' => 2]));
-echo PHP_EOL;
-
-print_r($route->call([4, 'b' => 2]));
-echo PHP_EOL;
-
 try {
+    print_r($route->call([4, 2]));
+    echo PHP_EOL;
+
+    print_r($route->call(['b' => 4, 'a' => 5]));
+    echo PHP_EOL;
+
+    print_r($route->call([4, 'b' => 2]));
+    echo PHP_EOL;
+
     print_r($route->call([4, 'b' => 'asas']));
     echo PHP_EOL;
 } catch (Throwable $t) {
