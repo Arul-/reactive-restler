cd14a76ab469482a9bd44e0f6df344a6847bf5f1
Arul Kumaran <arul@logicalsteps.net>
2019-05-27 11:50:21 +0800

    Removes unwanted method from AccessControl interface and implementation. Also makes AccessControl class explorable

--- api/examples/_010_access_control/AccessControl.php
+++ api/examples/_010_access_control/AccessControl.php
@@ -1,20 +1,32 @@
 <?php
 
 use Luracast\Restler\Contracts\AccessControlInterface;
+use Luracast\Restler\Contracts\ExplorableAuthenticationInterface;
 use Luracast\Restler\Contracts\SelectivePathsInterface;
 use Luracast\Restler\Contracts\SelectivePathsTrait;
 use Luracast\Restler\Contracts\UserIdentificationInterface;
-use Luracast\Restler\Data\Route;
 use Luracast\Restler\Exceptions\HttpException;
+use Luracast\Restler\OpenApi3\Security\ApiKeyAuth;
+use Luracast\Restler\OpenApi3\Security\Scheme;
+use Luracast\Restler\StaticProperties;
 use Luracast\Restler\Utils\ClassName;
 use Psr\Http\Message\ServerRequestInterface;
 
-class AccessControl implements AccessControlInterface, SelectivePathsInterface
+class AccessControl implements AccessControlInterface, SelectivePathsInterface, ExplorableAuthenticationInterface
 {
     use SelectivePathsTrait;
 
     public static $requires = 'user';
     public static $role = 'user';
+    /**
+     * @var StaticProperties
+     */
+    private $accessControl;
+
+    public function __construct(StaticProperties $accessControl)
+    {
+        $this->accessControl = $accessControl;
+    }
 
     /**
      * @param ServerRequestInterface $request
@@ -36,8 +48,8 @@ class AccessControl implements AccessControlInterface, SelectivePathsInterface
             return false;
         }
         $userClass::setCacheIdentifier($role);
-        static::$role = $role;
-        return static::$requires == $role || $role == 'admin';
+        $this->accessControl->role = $role;
+        return $this->accessControl->requires == $role || $role == 'admin';
     }
 
     public static function getWWWAuthenticateString(): string
@@ -45,11 +57,8 @@ class AccessControl implements AccessControlInterface, SelectivePathsInterface
         return 'Query name="api_key"';
     }
 
-    public static function verifyAccess(Route $route): bool
+    public static function scheme(): Scheme
     {
-        $requires = $route->requires ?? false;
-        return $requires
-            ? static::$role == 'admin' || static::$role == $requires
-            : true;
+        return new ApiKeyAuth('api_key', ApiKeyAuth::IN_QUERY);
     }
 }
--- src/Contracts/AccessControlInterface.php
+++ src/Contracts/AccessControlInterface.php
@@ -1,8 +1,5 @@
 <?php namespace Luracast\Restler\Contracts;
 
-use Luracast\Restler\Data\Route;
-
 interface AccessControlInterface extends AuthenticationInterface
 {
-    public static function verifyAccess(Route $route): bool;
 }
