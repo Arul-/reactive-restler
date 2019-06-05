62d34a1b78e7e5909afb8d08edbd27384d54169d
Arul Kumaran <arul@logicalsteps.net>
2019-06-05 16:42:36 +0800

    Let redirect class use the base path when relative path is used

--- src/Exceptions/Redirect.php
+++ src/Exceptions/Redirect.php
@@ -1,6 +1,9 @@
 <?php namespace Luracast\Restler\Exceptions;
 
 
+use Luracast\Restler\Router;
+use Luracast\Restler\Utils\Text;
+
 class Redirect extends HttpException
 {
     public function __construct(string $location, array $queryParams = [], int $httpStatusCode = 302)
@@ -8,6 +11,9 @@ class Redirect extends HttpException
         parent::__construct($httpStatusCode);
         if (!empty($queryParams)) {
             $location .= '?' . http_build_query($queryParams);
+        }
+        if (!Text::beginsWith($location, 'http') && !Text::beginsWith($location, '/')) {
+            $location = Router::getBasePath() . '/' . $location;
         }
         $this->setHeader('Location', $location);
     }
