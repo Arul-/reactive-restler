e6c4985ff8f21d3528f89b601c776cd2aa3b6885
Arul <arul@luracast.com>
2020-01-16 23:11:27 +0800

    Fixes and improvements to spreadsheet

--- src/MediaTypes/Spreadsheet.php
+++ src/MediaTypes/Spreadsheet.php
@@ -11,6 +11,7 @@ use Luracast\Restler\Contracts\ResponseMediaTypeInterface;
 use Luracast\Restler\Exceptions\HttpException;
 use Luracast\Restler\ResponseHeaders;
 use Luracast\Restler\Utils\Convert;
+use Luracast\Restler\Defaults;
 
 
 class Spreadsheet extends Dependent implements ResponseMediaTypeInterface
@@ -42,16 +43,22 @@ class Spreadsheet extends Dependent implements ResponseMediaTypeInterface
         $data = $this->convert->toArray($data);
         if (is_array($data) && array_values($data) == $data) {
             //if indexed array
+            $file = Defaults::$cacheDirectory . '/spreadsheet' . microtime() . '.' . $this->extension();
+            if (!is_writable($file)) {
+                throw new HttpException(500, 'Spreadsheet needs Defaults::$cacheDirectory to be writable.');
+            }
             $writer = WriterEntityFactory::createWriter($this->extension());
-            $file = Defaults::$cacheDirectory . '/phpxltmp' . microtime() . '.' . $this->extension();
             $writer->openToFile($file);
             $row = array_shift($data);
-            if (array_values($row) != $row) {
-                //write header
-                $style = (new StyleBuilder)->setFontBold()->setBackgroundColor(Color::rgb(30, 30, 30))->build();
+            $style = (new StyleBuilder)->setFontBold()->setBackgroundColor(Color::rgb(230, 230, 230))->build();
+            if (array_values($row) == $row) {
+                //write header with first row values
+                $writer->addRow(WriterEntityFactory::createRowFromArray(array_values($row), $style));
+            } else {
+                //write header with keys
                 $writer->addRow(WriterEntityFactory::createRowFromArray(array_keys($row), $style));
+                $writer->addRow(WriterEntityFactory::createRowFromArray(array_values($row)));
             }
-            $writer->addRow(WriterEntityFactory::createRowFromArray(array_values($row)));
             foreach ($data as $row) {
                 $writer->addRow(WriterEntityFactory::createRowFromArray(array_values($row)));
             }
