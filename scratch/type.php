0993968a70ca807fe6a06501162050d3fb604771
Arul <arul@luracast.com>
2020-02-22 10:02:39 +0530

    Type testing for a password string

--- scratch/type.php
+++ scratch/type.php
@@ -1,6 +1,8 @@
 <?php declare(strict_types=1);
 
+use Luracast\Restler\Data\Param;
 use Luracast\Restler\Data\Type;
+use Luracast\Restler\Utils\CommentParser;
 
 include __DIR__ . "/../vendor/autoload.php";
 
@@ -10,11 +12,27 @@ echo $type . PHP_EOL;
 
 class Test
 {
-    /** @var array {@type int|null} an array of integers */
-    public $obj;
+    /** @var null|array {@type integer|null} an array of integers */
+    public array $obj;
+
+    /**
+     * @param null|string $name {@type password}
+     * @return string|null
+     */
+    function welcome(string $name): ?string
+    {
+        return "welcome $name!";
+    }
 }
 
-echo (Type::fromProp(new ReflectionProperty(Test::class, 'obj'))).PHP_EOL;
+echo ($type = Type::fromProperty(new ReflectionProperty(Test::class, 'obj'))) . PHP_EOL;
+echo ($type = Type::__set_state($type->jsonSerialize())) . PHP_EOL;
+
+
+$method = new ReflectionMethod(Test::class, 'welcome');
+//var_dump(CommentParser::parse($method->getDocComment()));
+print_r(Param::fromFunction($method));
+
 /*
 
 $p = new ReflectionProperty(Type::class, 'format');
