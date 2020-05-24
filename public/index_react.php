ca826c72277cb9bf46b51da6a15ff767960bd25d
Arul <arul@luracast.com>
2020-05-24 15:02:38 +0800

    Fixes file upload for reactphp server

--- public/index_react.php
+++ public/index_react.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 require __DIR__ . '/../api/bootstrap.php';
 
@@ -7,6 +9,7 @@ use Luracast\Restler\Restler;
 use Psr\Http\Message\ServerRequestInterface;
 use React\Http\Middleware\LimitConcurrentRequestsMiddleware;
 use React\Http\Middleware\RequestBodyBufferMiddleware;
+use React\Http\Middleware\RequestBodyParserMiddleware;
 use React\Http\StreamingServer;
 
 
@@ -15,18 +18,24 @@ $loop = React\EventLoop\Factory::create();
 ReactHttpClient::setLoop($loop);
 Defaults::$implementations[HttpClientInterface::class] = [ReactHttpClient::class];
 
-$server = new StreamingServer([
-    new LimitConcurrentRequestsMiddleware(100), // 100 concurrent buffering handlers
-    new RequestBodyBufferMiddleware(16 * 1024 * 1024), // 16 MiB
-    function (ServerRequestInterface $request) {
-        echo '      ' . $request->getMethod() . ' ' . $request->getUri()->getPath() . PHP_EOL;
-        return (new Restler)->handle($request);
-    }
-]);
+$server = new StreamingServer(
+    [
+        new LimitConcurrentRequestsMiddleware(100), // 100 concurrent buffering handlers
+        new RequestBodyBufferMiddleware(16 * 1024 * 1024), // 16 MiB
+        new RequestBodyParserMiddleware(2 * 1024 * 1024, 1), //allow UPLOAD of only 1 file with max 2MB
+        function (ServerRequestInterface $request) {
+            echo '      ' . $request->getMethod() . ' ' . $request->getUri()->getPath() . PHP_EOL;
+            return (new Restler)->handle($request);
+        }
+    ]
+);
 
-$server->on('error', function (Exception $e) {
-    echo 'Error: ' . $e->getMessage() . PHP_EOL;
-});
+$server->on(
+    'error',
+    function (Exception $e) {
+        echo 'Error: ' . $e->getMessage() . PHP_EOL;
+    }
+);
 
 
 $socket = new React\Socket\Server(8080, $loop);
