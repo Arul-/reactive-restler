a808d14b58d016312bd5c569c9e8fd55e6ee0eed
Arul <arul@luracast.com>
2020-07-29 00:11:22 +0800

    Cleaning up Returns class

--- src/Data/Returns.php
+++ src/Data/Returns.php
@@ -9,37 +9,13 @@ use ReflectionNamedType;
 
 class Returns extends Type
 {
-    public static function fromReturnType(?ReflectionNamedType $type, ?array $metadata, array $scope): self
+    public static function fromReturnType(?ReflectionNamedType $reflectionType, ?array $metadata, array $scope): self
     {
         $instance = new static();
         $types = $metadata['type'] ?? ['array'];
-        $itemTypes = $metadata[CommentParser::$embeddedDataName]['type'] ?? ['string'];
+        $itemTypes = $metadata[CommentParser::$embeddedDataName]['type'] ?? ['object'];
         $instance->description = $metadata['description'] ?? '';
-        $instance->apply($type, $types, $itemTypes, $scope);
+        $instance->apply($reflectionType, $types, $itemTypes, $scope);
         return $instance;
     }
-
-    public static function parse(array $properties): self
-    {
-        $p2 = $properties[CommentParser::$embeddedDataName] ?? [];
-        unset($properties[CommentParser::$embeddedDataName]);
-        if (isset($p2['type'])) {
-            $p2['contentType'] = $p2['type'];
-            unset($p2['type']);
-        }
-
-        $instance = new static();
-        $instance->applyProperties($properties);
-        $instance->applyProperties($p2);
-        if (is_array($instance->children)) {
-            foreach ($instance->children as $key => $child) {
-                if (!$child instanceof static) {
-                    $instance->children[$key] = static::parse($child);
-                }
-            }
-        }
-        $instance->updateFlags();
-        return $instance;
-    }
-
 }
