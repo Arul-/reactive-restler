9569bcaa0ac029ab46a7df0fd53c1792d2657493
Arul <arul@luracast.com>
2020-08-11 11:04:03 +0800

    Supports documenting pagination

---
+++ src/Data/PaginatedResponse.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace Luracast\Restler\Data;
+
+
+use JsonSerializable;
+use Luracast\Restler\Contracts\GenericResponseInterface;
+use ReflectionClass;
+
+class PaginatedResponse implements GenericResponseInterface
+{
+    /**
+     * @var JsonSerializable
+     */
+    private $serializable;
+
+    public function __construct(JsonSerializable $serializable)
+    {
+        $this->serializable = $serializable;
+    }
+
+    public static function responds(string ...$types): Returns
+    {
+        return Returns::__set_state([
+            'type' => 'object',
+            'properties' => [
+                'current_page' => Returns::__set_state(['type' => 'int']),
+                'data' => empty($types)
+                    ? Returns::__set_state(['type' => 'object', 'scalar' => false])
+                    : Returns::fromClass(new ReflectionClass($types[0])),
+                'first_page_url' => Returns::__set_state(['type' => 'string']),
+                'from' => Returns::__set_state(['type' => 'int']),
+                'last_page' => Returns::__set_state(['type' => 'int']),
+                'last_page_url' => Returns::__set_state(['type' => 'string']),
+                'next_page_url' => Returns::__set_state(['type' => 'string']),
+                'path' => Returns::__set_state(['type' => 'string']),
+                'per_page' => Returns::__set_state(['type' => 'int']),
+                'prev_page_url' => Returns::__set_state(['type' => 'string']),
+                'to' => Returns::__set_state(['type' => 'int']),
+                'total' => Returns::__set_state(['type' => 'int']),
+            ]
+        ]);
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->serializable->jsonSerialize();
+    }
+}
